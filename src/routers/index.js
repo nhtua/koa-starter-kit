12f1135d3a0a8d2bf8eec6f5301bca9091db2c6e
nhtua <hoangtua.vn@gmail.com>
2018-07-23 13:36:38 +0700

    auto import new route

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,12 +1,14 @@
+import glob from 'glob';
+import path from 'path';
 import compose from 'koa-compose';
-import routerAuth from './router-auth';
-import routerUser from './router-user';
 
 //We need to convert list of separated route in to the one
-let routers = [
-  routerAuth,
-  routerUser
-];
+//The block below does dynamic import all routers inside current directory.
+let routers = [];
+glob.sync( path.join(__dirname,'router-*.js') ).forEach( function( file ) {
+  let r = require( path.resolve( file ) ).default;
+  routers.push(r);
+});
 
 //So we extract the middelware from router
 let middleware = [];
